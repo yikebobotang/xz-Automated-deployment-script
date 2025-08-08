05c9065d2791c5c5938e7d993f6c2ae806430861
zlx <zlx>
2025-08-08 18:46:15 +0800

    docker-harbor-k8s

--- start.sh
+++ start.sh
@@ -5,13 +5,23 @@ init_server() {
     ansible-playbook -i host/hosts --tags="init" setup.yaml -f 20
 }
 
+install_docker() {
+    ansible-playbook -i host/hosts --tags="docker" setup.yaml -f 20
+}
+
+install_harbor() {
+    ansible-playbook -i host/hosts --tags="harbor" setup.yaml -f 20
+}
+
 menu_list() {
     echo "##################################################"
     echo "#                  一键部署脚本                   #"
     echo "##################################################"
     echo "      [1]   所有主机进行系统初始化操作              "
+    echo "      [2]   安装docker                              "
+    echo "      [3]   安装harbor                              "
     echo "##################################################"
-    read -r -p "请输入你的编号来选择进行安装【0-14】: " number
+    read -r -p "请输入你的编号来选择进行安装【0-2】: " number
 }
 
 main() {
@@ -22,6 +32,12 @@ main() {
             case $number in
             1)
                 init_server
+                ;;
+            2)
+                install_docker
+                ;;
+            3)
+                install_harbor
                 ;;
             Q|q)
                 break
